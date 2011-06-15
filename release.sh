9fe1393a3d6b2148faa69a73d04369b42f392c3c
Stanislas Polu <stan@bipsly.com>
2011-06-15 10:06:33 +0200

    added url

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 echo 'Releasing Bipsly 0.1.1'
 rm bipsly-*.js
-cat js/lib/jquery.cookie.js js/lib/jquery.json-2.2.js js/bipsly.js js/pipe.js js/cohort.js > bipsly-0.1.1.js
+cat js/lib/jquery.cookie.js js/lib/jquery.url.js js/lib/jquery.json-2.2.js js/bipsly.js js/pipe.js js/cohort.js > bipsly-0.1.1.js
 echo 'Done'
